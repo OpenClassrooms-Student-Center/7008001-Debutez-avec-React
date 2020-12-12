50e471aedba677c13167f6d2ae6ab3fce1c95d0e
Alexia Toulmet <alexia.toulmet@gmail.com>
2020-12-13 00:07:19 +0100

    Add plantlist css

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,5 @@
 import { plantList } from '../datas/plantList'
+import '../styles/ShoppingList.css'
 
 function ShoppingList() {
 	const categories = plantList.reduce(
@@ -14,9 +15,11 @@ function ShoppingList() {
 					<li key={cat}>{cat}</li>
 				))}
 			</ul>
-			<ul>
+			<ul className='lmj-plant-list'>
 				{plantList.map((plant) => (
-					<li key={plant.id}>{plant.name}</li>
+					<li key={plant.id} className='lmj-plant-item'>
+						{plant.name}
+					</li>
 				))}
 			</ul>
 		</div>
--- src/datas/plantList.js
+++ src/datas/plantList.js
@@ -2,26 +2,23 @@ export const plantList = [
 	{
 		name: 'monstera',
 		category: 'classique',
-		id: '1ed',
-		isAvailable: true
+		id: '1ed'
 	},
 	{
 		name: 'ficus lyrata',
 		category: 'classique',
-		id: '2ab',
-		isAvailable: false
+		id: '2ab'
 	},
 	{
 		name: 'pothos argenté',
 		category: 'classique',
 		id: '3sd',
-		isAvailable: true
+		specialOffer: true
 	},
 	{
 		name: 'yucca',
 		category: 'classique',
-		id: '4kk',
-		isAvailable: true
+		id: '4kk'
 	},
 	{
 		name: 'olivier',
@@ -32,25 +29,23 @@ export const plantList = [
 	{
 		name: 'géranium',
 		category: 'extérieur',
-		id: '6uo',
-		isAvailable: false
+		id: '6uo'
 	},
 	{
 		name: 'basilique',
 		category: 'extérieur',
 		id: '7ie',
-		isAvailable: true
+		specialOffer: true
 	},
 	{
 		name: 'aloe',
 		category: 'plante grasse',
 		id: '8fp',
-		isAvailable: true
+		specialOffer: true
 	},
 	{
 		name: 'succulente',
 		category: 'plante grasse',
-		id: '9vn',
-		isAvailable: true
+		id: '9vn'
 	}
 ]
