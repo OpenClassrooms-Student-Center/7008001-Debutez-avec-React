954b805e99d385dca49054e8fcd06be2dac45b17
Alexia Toulmet <alexia.toulmet@gmail.com>
2020-12-14 18:23:19 +0100

    create P2C5 solution

--- src/components/Care.js
+++ src/components/Care.js
@@ -1,7 +1,15 @@
 import Sun from '../assets/sun.svg'
 import Water from '../assets/water.svg'
 
-function Care({ care, careType }) {
+// Ici, il s'agit d'une manière de faire.
+//Vous auriez aussi pu utiliser une fonction qui retourne l'élément souhaité, ou bien faire directement des conditions
+const quantityLabel = {
+	1: 'peu',
+	2: 'modérément',
+	3: 'beaucoup'
+}
+
+function Care({ value, careType }) {
 	const scale = [1, 2, 3]
 	const scaleType =
 		careType === 'light' ? (
@@ -11,9 +19,17 @@ function Care({ care, careType }) {
 		)
 
 	return (
-		<div>
+		<div
+			onClick={() =>
+				alert(
+					`Cette plante requiert ${quantityLabel[value]} ${
+						careType === 'light' ? 'de lumière' : "d'arrosage"
+					}`
+				)
+			}
+		>
 			{scale.map((elem) =>
-				care >= elem ? <span key={elem.toString()}>{scaleType}</span> : null
+				value >= elem ? <span key={elem.toString()}>{scaleType}</span> : null
 			)}
 		</div>
 	)
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,31 @@
+import { useState } from 'react'
 import '../styles/Footer.css'
 
 function Footer() {
+	const [inputValue, setInputValue] = useState('')
+
+	function handleInput(e) {
+		setInputValue(e.target.value)
+	}
+
+	function handleBlur() {
+		if (!inputValue.includes('@')) {
+			alert("Attention, il n'y a pas d'@, ceci n'est pas une adresse valide 😥")
+		}
+	}
+
 	return (
 		<footer className='lmj-footer'>
 			<div className='lmj-footer-elem'>
 				Pour les passionné·e·s de plantes 🌿🌱🌵
 			</div>
 			<div className='lmj-footer-elem'>Laissez-nous votre mail :</div>
+			<input
+				placeholder='Entrez votre mail'
+				onChange={handleInput}
+				value={inputValue}
+				onBlur={handleBlur}
+			/>
 		</footer>
 	)
 }
