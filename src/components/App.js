95b38755c7aa2491964d05d05729107801a4506f
Alexia Toulmet <alexia.toulmet@gmail.com>
2020-12-14 17:57:21 +0100

    add footer + style

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,20 @@
 import Banner from './Banner'
 import logo from '../assets/logo.png'
 // import Cart from './Cart'
-import QuestionForm from './QuestionForm'
+import Footer from './Footer'
 import ShoppingList from './ShoppingList'
 
 function App() {
 	return (
 		<div>
 			<Banner>
-				<img src={logo} alt='logo-la-maison-jungle' />
+				<img src={logo} alt='logo-la-maison-jungle' className='lmj-logo' />
 				<h1 className='lmj-title'>La maison jungle</h1>
 			</Banner>
-			<QuestionForm />
 
 			{/* <Cart /> */}
 			<ShoppingList />
+			<Footer />
 		</div>
 	)
 }
