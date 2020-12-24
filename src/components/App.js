fd0edb47b1ce4efd0998fc1306a7f6bde180f41b
Alexia Toulmet <alexia.toulmet@gmail.com>
2020-12-24 01:17:33 +0100

    delete isBestSale

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ function App() {
 	return (
 		<div>
 			<Banner>
-				<img src={logo} alt='logo-la-maison-jungle' className='lmj-logo' />
+				<img src={logo} alt='La maison jungle' className='lmj-logo' />
 				<h1 className='lmj-title'>La maison jungle</h1>
 			</Banner>
 
