c5cb2ba2e5ab450faf87af3822f293d1d4983780
Alexia Toulmet <alexia.toulmet@gmail.com>
2020-12-27 12:53:38 +0100

    change screencast solution to array

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -3,9 +3,8 @@ import '../styles/Cart.css'
 
 function Cart({ cart, updateCart }) {
 	const [isOpen, setIsOpen] = useState(true)
-	const items = Object.keys(cart)
-	const total = items.reduce(
-		(acc, item) => acc + cart[item].amount * cart[item].price,
+	const total = cart.reduce(
+		(acc, plantType) => acc + plantType.amount * plantType.price,
 		0
 	)
 	return isOpen ? (
@@ -16,18 +15,18 @@ function Cart({ cart, updateCart }) {
 			>
 				Fermer
 			</button>
-			{items.length > 0 ? (
+			{cart.length > 0 ? (
 				<div>
 					<h2>Panier</h2>
 					<ul>
-						{items.map((item) => (
-							<li key={item}>
-								{cart[item]?.amount} {item} {cart[item]?.price}€
-							</li>
+						{cart.map(({ name, price, amount }, index) => (
+							<div key={`${name}-${index}`}>
+								{name} {price}€ x {amount}
+							</div>
 						))}
 					</ul>
 					<h3>Total :{total}€</h3>
-					<button onClick={() => updateCart({})}>Vider le panier</button>
+					<button onClick={() => updateCart([])}>Vider le panier</button>
 				</div>
 			) : (
 				<div>Votre panier est vide</div>
