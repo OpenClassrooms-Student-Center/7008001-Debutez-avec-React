290074cdce3eb3fdf84e7ee5fc6636608d247511
Alexia Toulmet <alexia.toulmet@gmail.com>
2020-12-27 04:16:17 +0100

    add inner code

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,12 +1,14 @@
 import logo from '../assets/logo.png'
 import '../styles/Banner.css'
+import Recommendation from './Recommendation'
 
 function Banner() {
 	const title = 'La maison jungle'
 	return (
 		<div className='lmj-banner'>
-			<img src={logo} alt='La maison jungle' />
+			<img src={logo} alt='La maison jungle' className='lmj-logo' />
 			<h1 className='lmj-title'>{title}</h1>
+			<Recommendation />
 		</div>
 	)
 }
