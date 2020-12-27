4807a96aca1de041f9e739f65d97a1fe979d06b2
Alexia Toulmet <alexia.toulmet@gmail.com>
2020-12-27 12:57:09 +0100

    change screencast solution to array

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -20,18 +20,18 @@ function Cart({ cart, updateCart }) {
 			>
 				Fermer
 			</button>
-			{items.length > 0 ? (
+			{cart.length > 0 ? (
 				<div>
 					<h2>Panier</h2>
 					<ul>
-						{items.map((item) => (
-							<li key={item}>
-								{cart[item]?.amount} {item} {cart[item]?.price}€
-							</li>
+						{cart.map(({ name, price, amount }, index) => (
+							<div key={`${name}-${index}`}>
+								{name} {price}€ x {amount}
+							</div>
 						))}
 					</ul>
 					<h3>Total :{total}€</h3>
-					<button onClick={() => updateCart({})}>Vider le panier</button>
+					<button onClick={() => updateCart([])}>Vider le panier</button>
 				</div>
 			) : (
 				<div>Votre panier est vide</div>
