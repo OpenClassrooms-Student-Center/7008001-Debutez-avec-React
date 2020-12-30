11aeacaa65069ce73e8943f25522d31fb4ed53bc
Alexia Toulmet <alexia.toulmet@gmail.com>
2020-12-30 18:03:01 +0100

    change care naming + delete isBestSale

--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -1,14 +1,14 @@
-import Care from './Care'
+import CareScale from './CareScale'
 import '../styles/PlantItem.css'
 
-function PlantItem({ id, cover, name, isBestSale, water, light }) {
+function PlantItem({ id, cover, name, water, light }) {
 	return (
 		<li key={id} className='lmj-plant-item'>
 			<img className='lmj-plant-item-cover' src={cover} alt={`${name} cover`} />
 			{name}
 			<div>
-				<Care careType='water' value={water} />
-				<Care careType='light' value={light} />
+				<CareScale careType='water' scaleValue={water} />
+				<CareScale careType='light' scaleValue={light} />
 			</div>
 		</li>
 	)
