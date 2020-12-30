f73d8f89ff0a6b05a485bb980fb6f56c2fb725cc
Alexia Toulmet <alexia.toulmet@gmail.com>
2020-12-30 18:22:53 +0100

    change care naming

--- src/components/CareScale.js
+++ src/components/CareScale.js
@@ -1,8 +1,8 @@
 import Sun from '../assets/sun.svg'
 import Water from '../assets/water.svg'
 
-function Care({ care, careType }) {
-	const scale = [1, 2, 3]
+function CareScale({ scaleValue, careType }) {
+	const range = [1, 2, 3]
 	const scaleType =
 		careType === 'light' ? (
 			<img src={Sun} alt='sun-icon' />
@@ -12,11 +12,13 @@ function Care({ care, careType }) {
 
 	return (
 		<div>
-			{scale.map((elem) =>
-				care >= elem ? <span key={elem.toString()}>{scaleType}</span> : null
+			{range.map((rangeElem) =>
+				scaleValue >= rangeElem ? (
+					<span key={rangeElem.toString()}>{scaleType}</span>
+				) : null
 			)}
 		</div>
 	)
 }
 
-export default Care
+export default CareScale
