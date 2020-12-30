fa90e85270bb7dba321a773077cfc306bab7e49b
Alexia Toulmet <alexia.toulmet@gmail.com>
2020-12-30 17:57:55 +0100

    change care naming

---
+++ src/components/CareScale.js
@@ -0,0 +1,16 @@
+function CareScale({ scaleValue, careType }) {
+	const range = [1, 2, 3]
+	const scaleType = careType === 'light' ? '☀️' : '💧'
+
+	return (
+		<div>
+			{range.map((rangeElem) =>
+				scaleValue >= rangeElem ? (
+					<span key={rangeElem.toString()}>{scaleType}</span>
+				) : null
+			)}
+		</div>
+	)
+}
+
+export default CareScale
