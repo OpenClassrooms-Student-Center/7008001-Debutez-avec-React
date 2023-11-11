8b9ad094bab54fa651c17cb60d44d188c5e8d2da
Wadie <142181281+ouadia-1@users.noreply.github.com>
2023-11-11 20:50:25 +0100

    Update Cart.js

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -3,7 +3,7 @@ function Cart() {
 	const ivyPrice = 10
 	const flowerPrice = 15
 	return (
-		<div>
+		<div className="lmj-cart">
 			<h2>Panier</h2>
 			<ul>
 				<li>Monstera : {monsteraPrice}€</li>
